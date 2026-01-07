972ec900799e149dec4fbd55e299bc92fea60ccd
Albert Ritmeester <644662+aritmeester@users.noreply.github.com>
2026-01-07 16:12:05 +0100

    Optimized config flow

--- README.md
+++ README.md
@@ -40,6 +40,8 @@ Copy `custom_components/tennet_balance` into your Home Assistant config director
 You need:
 - A TenneT API key
 
+An API key can be created via https://developer.tennet.eu/api-keys.\n\nMake sure the API key is created for the selected environment.\n\nTo create an API key, a developer account is required, which can be requested via https://developer.tennet.eu/register/. Approval of a developer account may take several days.
+
 Configuration is done via the UI.
 
 ## Sensors
--- custom_components/tennet_balance/config_flow.py
+++ custom_components/tennet_balance/config_flow.py
@@ -9,10 +9,17 @@ class TennetConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="TenneT Balance Delta", data=user_input)
 
+        ENV_LABELS = {
+            "api": "Production",
+            "api.acc": "Acceptance"
+        }
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("api_key"): str,
-                vol.Required("environment", default="api"): vol.In(["api", "api.acc"]),
+                vol.Required("environment", default="api"): vol.In({k: v for k, v in ENV_LABELS.items()}),
+                vol.Required("api_key", description={"suggested_value": ""}): str,
             }),
+            description_placeholders={
+                "api_key": "An API key can be created via https://developer.tennet.eu/api-keys.\n\nMake sure the API key is created for the selected environment.\n\nTo create an API key, a developer account is required, which can be requested via https://developer.tennet.eu/register/. Approval of a developer account may take several days."
+            }
         )
